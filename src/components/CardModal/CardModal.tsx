889bdf8233dd9dd8b0cd0bb7c7bdf1f3f519315e
Vyacheslav Kulik <kulik.vyacheslav94@gmail.com>
2020-11-03 18:50:45 +0600

    Add toogle in PopUp Cards

--- src/components/CardModal/CardModal.tsx
+++ src/components/CardModal/CardModal.tsx
@@ -1,11 +1,10 @@
-import React, { useCallback } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
+import React, {useCallback, useState} from 'react';
+import {useHistory, useParams} from 'react-router-dom';
 import './CardModal.scss';
 import close from './close.svg';
 import cardIcon from './card.svg';
 import description from './description.svg';
 import commentsIcon from './comments.svg';
-import { CommentsModal } from 'components/CommentsModal';
 import {ICards, IComments, ILists, RenderCommentsModal} from "../../interfaces/interfaces";
 
 interface CardModalProps {
@@ -19,30 +18,48 @@ interface ParamTypes {
     id: string
 }
 
-export const CardModal: React.FC<CardModalProps> = ({ cards, comments, lists, render }) => {
+export const CardModal: React.FC<CardModalProps> = ({cards, comments, lists, render}) => {
 
     const history = useHistory();
     const params = useParams<ParamTypes>();
     const card: ICards | undefined = cards.find(card => card.id === params.id);
     const commentsCard = comments.filter(comment => comment.idCard === params.id);
-    const listCards = lists.find( list => list.id === card?.idList );
+    const listCards = lists.find(list => list.id === card?.idList);
+
+    const [toggleTitleCard, setToggleTitleCard] = useState(false);
+    const [toggleDescCard, setToggleDescCard] = useState(false);
+
 
     const handlerCloseModal = useCallback(() => {
         history.goBack();
     }, [history]);
 
+    const handlerToggleCards = useCallback((event: React.MouseEvent<HTMLDivElement>) => {
+        event.stopPropagation();
+        setToggleTitleCard(true);
+
+    }, []);
+
+    const handlerToggleCardsReset = useCallback(() => {
+        (toggleTitleCard && setToggleTitleCard(false));
+        (toggleDescCard && setToggleDescCard(false));
+    }, [toggleTitleCard, toggleDescCard]);
 
     return (
         <div className="card-modal">
 
-            <div className="card-modal__content">
+            <div onClick={handlerToggleCardsReset} className="card-modal__content">
                 <div onClick={handlerCloseModal} className="card-modal__close">
                     <img src={close} alt="close"/>
                 </div>
                 <div className="card-modal__data">
                     <div className="card-modal__title">
-                        <img src={cardIcon} alt="close" className="card-modal__img" />
-                        <h3>{card?.title}</h3>
+                        <img src={cardIcon} alt="close" className="card-modal__img"/>
+                        {toggleTitleCard ?
+                            <input onClick={(el) => el.stopPropagation()} type="text" defaultValue={card?.title}/> :
+                            <h3 onClick={handlerToggleCards}>{card?.title}</h3>
+                        }
+
                     </div>
                     <div className="card-modal__list-title">
                         <h6>in list {listCards?.title}</h6>
@@ -52,7 +69,10 @@ export const CardModal: React.FC<CardModalProps> = ({ cards, comments, lists, re
                             <img src={description} alt="description" className="card-modal__img"/>
                             <h6>Description</h6>
                         </div>
-                        <p className="card-modal__description-field card-modal_ml-img">{card?.description}</p>
+                        {toggleDescCard ?
+                            <textarea onClick={(el) => el.stopPropagation()} className="card-modal__description-field card-modal_ml-img" defaultValue={card?.description}/> :
+                            <p onClick={() => setToggleDescCard(true)} className="card-modal__description-field card-modal_ml-img">{card?.description}</p>}
+
                     </div>
                     <div className="card-modal__comment">
                         <div className="card-modal__comment-title">
@@ -67,7 +87,7 @@ export const CardModal: React.FC<CardModalProps> = ({ cards, comments, lists, re
                         </div>
                         <div className="card-modal__comment-field">
                             {commentsCard.length > 0 &&
-                            commentsCard.map( (comment) => render(comment.id, comment))
+                            commentsCard.map((comment) => render(comment.id, comment))
                             }
                         </div>
                     </div>
